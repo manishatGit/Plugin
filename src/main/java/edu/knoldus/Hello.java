9ddeaf85fe5358424f9c763836954f6bff8bf76e
randhir <randhir.kumar@knoldus.in>
2019-02-11 14:24:41 +0530

    implemented testing plugin

---
+++ src/main/java/edu/knoldus/Hello.java
@@ -0,0 +1,18 @@
+package edu.knoldus;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+@Mojo(name = "test", threadSafe = true)
+public class Hello extends AbstractMojo {
+
+    @Parameter(name = "test.message", required = true)
+    public String message;
+
+    public void execute() throws MojoExecutionException {
+        System.out.println("Hello "+ message);
+    }
+
+}
